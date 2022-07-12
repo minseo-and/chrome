8a82f8660b811d3aadb961af9a7e6068b665c093
minseo <msp05070478@gmail.com>
2022-07-12 09:45:48 +0900

    windowResize

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const title = document.getElementById("title")
-console.dir(title)
-title.innerText = "Got you!"
+const h1 = document.querySelector(".hello hi")
+
+function handleTitleClick() {
+    const currentColor = h1.style.color
+    let newColor
+    if(currentColor === "blue"){
+        newColor= "tomato"
+    } else {
+        newColor = "blue"
+    }
+    h1.style.color = newColor
+}
+
+function handleWindowResize() {
+    const currentColor2 = document.body.style.backgroundColor
+    let newColor2
+    if(currentColor2 === "blue"){
+        newColor2= "tomato"
+    } else {
+        newColor2 = "blue"
+    }
+    document.body.style.backgroundColor = newColor2
+}
+h1.addEventListener("click", handleTitleClick)
+
+window.addEventListener("resize", handleWindowResize)
